1ea4ce2d8596462b9de30fd66433e2764b3a4bf6
chiranjeevi583 <gcpchiranjeevi@gmail.com>
2025-04-13 22:06:41 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,42 @@
+provider "google" {
+ project = "gcp-dev-space"
+ region = "us-central1"
+ zone = "us-central1-a"
+}
+resource "google_compute_network" "vpc_network" {
+ name = "my-vpc"
+ auto_create_subnetworks = false
+}
+resource "google_compute_subnetwork" "subnet" {
+ name = "my-subnet"
+ ip_cidr_range = "10.10.0.0/24"
+ region = "us-central1"
+ network = google_compute_network.vpc_network.id
+}
+resource "google_compute_firewall" "firewall" {
+ name = "allow-ssh-http"
+ network = google_compute_network.vpc_network.name
+ allow {
+ protocol = "tcp"
+ ports = ["22", "80"]
+ }
+ source_ranges = ["0.0.0.0/0"]
+}
+resource "google_compute_instance" "vm_instance" {
+ name = "my-vm"
+ machine_type = "e2-medium"
+ zone = "us-central1-a"
+ boot_disk {
+ initialize_params {
+ image = "debian-cloud/debian-11"
+ }
+ }
+ network_interface {
+ network = google_compute_network.vpc_network.name
+ subnetwork = google_compute_subnetwork.subnet.name
+ access_config {
+ // Enables external IP
+ }
+ }
+ tags = ["http-server", "ssh"]
+}
