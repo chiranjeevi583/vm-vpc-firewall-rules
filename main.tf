07e9c4a9b257e2c2789a3aa3b36260c51d6ddbc3
chiranjeevi583 <gcpchiranjeevi@gmail.com>
2025-04-13 23:26:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,20 @@
 provider "google" {
  project = "gcp-dev-space"
  region = "us-central1"
- zone = "us-central1-a"
+ zone = "us-central1-b"
 }
 resource "google_compute_network" "vpc_network" {
- name = "my-vpc"
+ name = "my-vpc1"
  auto_create_subnetworks = false
 }
 resource "google_compute_subnetwork" "subnet" {
- name = "my-subnet"
+ name = "my-subnet1"
  ip_cidr_range = "10.10.0.0/24"
  region = "us-central1"
  network = google_compute_network.vpc_network.id
 }
 resource "google_compute_firewall" "firewall" {
- name = "allow-ssh-http"
+ name = "allow-ssh-https"
  network = google_compute_network.vpc_network.name
  allow {
  protocol = "tcp"
@@ -23,7 +23,7 @@ resource "google_compute_firewall" "firewall" {
  source_ranges = ["0.0.0.0/0"]
 }
 resource "google_compute_instance" "vm_instance" {
- name = "my-vm"
+ name = "my-vm1"
  machine_type = "e2-medium"
  zone = "us-central1-a"
  boot_disk {
